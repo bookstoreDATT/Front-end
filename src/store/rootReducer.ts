6e9add4d0e890b1e0481cc02703ad6ea2a325081
nguyennhuyenn <nguyennhuyen384@gmail.com>
2025-02-11 22:55:46 +0700

    chore: update root reducers

--- src/store/rootReducer.ts
+++ src/store/rootReducer.ts
@@ -1,8 +1,5 @@
 import { combineReducers } from '@reduxjs/toolkit';
-import theme from '~/store/slice/themeSlice';
 
-const rootReducer = combineReducers({
-    
-});
+const rootReducer = combineReducers({});
 
 export default rootReducer;
